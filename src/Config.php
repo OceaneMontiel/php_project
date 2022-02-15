db40efa205c82b2be15afbee7a76e0af5cd5bd79
oceane <oceane.montiel@laposte.net>
2022-02-15 09:09:59 +0100

    config.php

---
+++ src/Config.php
@@ -0,0 +1,12 @@
+<?php
+
+class Config {
+
+    private int $id;
+    private string $name;
+    
+    private Box $box;
+    private Motherboard $motherboard;
+    private Processor $processor;
+    private Ram $ram;
+}
